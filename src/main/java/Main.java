b97df1104ac0bfb75908228e94c8f234e3ae8676
Kiluadox-Ankit22 <ankityd777@gmail.com>
2026-08-13 12:51:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage;
         System.out.print("$ ");
+
+        Scanner sc = new Scanner(System.in);
+        string command = sc.nextline;
+
+        System.out.println(command + ": command not found");
     }
 }
