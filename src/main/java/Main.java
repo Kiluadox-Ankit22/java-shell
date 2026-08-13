de25859582d5e7b999e0daf5061f7afba41debe2
Kiluadox-Ankit22 <ankityd777@gmail.com>
2026-08-13 13:00:25 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
         // TODO: Uncomment the code below to pass the first stage;
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
 
         System.out.println(command + ": command not found");
     }
