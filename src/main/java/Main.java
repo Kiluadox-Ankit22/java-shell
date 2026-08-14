ed8f2c4c5b15abbd4b59087c3608203fc99dc338
Kiluadox-Ankit22 <ankityd777@gmail.com>
2026-08-14 15:35:49 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
-        if (input.equals("exit")){
+        if (command.equals("exit")){
             break;
         }
         System.out.println(command + ": command not found");
